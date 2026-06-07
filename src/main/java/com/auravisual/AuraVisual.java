ebc7432607e3eea1fdfdf117432360084d42ec26
eastory2k <eastory2k@mail.ru>
2026-06-08 00:35:09 +0300

    Create AuraVisual.java

---
+++ src/main/java/com/auravisual/AuraVisual.java
@@ -0,0 +1,15 @@
+package com.auravisual;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuraVisual implements ModInitializer {
+    public static final String MOD_ID = "auravisual";
+    public static final Logger LOGGER = LoggerFactory.LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("AuraVisual приветствует тебя! Мод успешно запущен.");
+    }
+}
