00af488d11953d17c0a97a16a26f8c9e59b6e144
eastory2k <eastory2k@mail.ru>
2026-06-08 01:00:21 +0300

    Update AuraVisual.java

--- src/main/java/com/auravisual/AuraVisual.java
+++ src/main/java/com/auravisual/AuraVisual.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class AuraVisual implements ModInitializer {
     public static final String MOD_ID = "auravisual";
-    public static final Logger LOGGER = LoggerFactory.LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
