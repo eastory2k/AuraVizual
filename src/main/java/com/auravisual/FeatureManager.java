daafa441466d60ac4cd01c1c74fd22d1ca62d68d
eastory2k <eastory2k@mail.ru>
2026-06-08 01:32:49 +0300

    Create FeatureManager.java

---
+++ src/main/java/com/auravisual/FeatureManager.java
@@ -0,0 +1,12 @@
+package com.auravisual;
+
+public class FeatureManager {
+    // Наши модули (переключатели)
+    public static boolean itemSwapVisual = true;
+    public static boolean targetHUD = true;
+    public static boolean dynamicIsland = false;
+    
+    // Настройки цвета (в формате ARGB Hex)
+    // 0xFF8A2BE2 — фирменный фиолетовый цвет Ауры (Neon Purple)
+    public static int clientColor = 0xFF8A2BE2; 
+}
