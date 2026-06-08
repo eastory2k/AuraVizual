c8716665baf35a71dadeaa0719e8802870463e87
eastory2k <eastory2k@mail.ru>
2026-06-08 19:12:27 +0300

    Update AuraVisual.java

--- src/main/java/com/auravisual/AuraVisual.java
+++ src/main/java/com/auravisual/AuraVisual.java
@@ -6,9 +6,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 public class AuraVisual implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Подключаем игровой цикл строго на клиенте, чтобы не крашить лаунчер
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            AuraVisualClient.onClientTick(client);
-        });
+        // Логика вынесена в Mixin для более стабильного FPS, но точку входа оставляем пустой для работы Fabric
     }
 }
