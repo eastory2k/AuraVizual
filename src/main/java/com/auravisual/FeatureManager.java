6d999bb5d77e9dfd4c5efa5003240272fa6bf7b2
eastory2k <eastory2k@mail.ru>
2026-06-08 12:49:44 +0300

    Update FeatureManager.java

--- src/main/java/com/auravisual/FeatureManager.java
+++ src/main/java/com/auravisual/FeatureManager.java
@@ -1,7 +1,9 @@
 package com.auravisual;
 
+import java.util.Random;
+
 public class FeatureManager {
-    public static int clientColor = 0xFFB266FF; // Фиолетовый по умолчанию
+    public static int clientColor = 0xFFB266FF; 
     
     // Состояния модулей
     public static boolean targetHUD = false;
@@ -11,21 +13,23 @@ public class FeatureManager {
     public static boolean glowESP = false;
     public static boolean customParticles = false;
     public static boolean itemSwapVisual = false;
-    
-    // НАША НОВАЯ ПЕРЕМЕННАЯ
     public static boolean soulSight = false; 
+    
+    // НАШ ОБНОВЛЕННЫЙ ТРИГГЕРБOТ
+    public static boolean triggerBot = false; 
+    public static int triggerDelayTicks = 0; // Текущая задержка в тиках
+    public static final Random random = new Random(); // Генератор случайных чисел
 
     // Косметика
     public static boolean showWings = false;
     public static boolean showHat = false;
     public static boolean showDemonicRays = false;
 
-    // Метод для смены цвета (Color Picker)
     public static void toggleColor() {
-        if (clientColor == 0xFFB266FF) clientColor = 0xFFFF3333; // В красный
-        else if (clientColor == 0xFFFF3333) clientColor = 0xFF33FF33; // В зеленый
-        else if (clientColor == 0xFF33FF33) clientColor = 0xFF33CCFF; // В голубой
-        else clientColor = 0xFFB266FF; // Назад в фиолетовый
+        if (clientColor == 0xFFB266FF) clientColor = 0xFFFF3333; 
+        else if (clientColor == 0xFFFF3333) clientColor = 0xFF33FF33; 
+        else if (clientColor == 0xFF33FF33) clientColor = 0xFF33CCFF; 
+        else clientColor = 0xFFB266FF; 
     }
 
     public static String getColorName() {
