3cb5f341b93f4e58dd82ec430ff5359d6f9752e2
eastory2k <eastory2k@mail.ru>
2026-06-08 16:39:53 +0300

    Update FeatureManager.java

--- src/main/java/com/auravisual/FeatureManager.java
+++ src/main/java/com/auravisual/FeatureManager.java
@@ -1,42 +1,21 @@
 package com.auravisual;
 
-import java.util.Random;
-
 public class FeatureManager {
-    public static int clientColor = 0xFFB266FF; 
+    public static int clientColor = 0xFFFF0000; // Красный цвет по умолчанию
     
-    // Состояния модулей
+    // Переключатели функций (все независимы)
+    public static boolean triggerBot = false;
     public static boolean targetHUD = false;
-    public static int targetHudMode = 1;
     public static boolean armorHUD = false;
     public static boolean potionHUD = false;
     public static boolean glowESP = false;
+    public static boolean soulSight = false;
     public static boolean customParticles = false;
     public static boolean itemSwapVisual = false;
-    public static boolean soulSight = false; 
-    
-    // НАШ ОБНОВЛЕННЫЙ ТРИГГЕРБOТ
-    public static boolean triggerBot = false; 
-    public static int triggerDelayTicks = 0; // Текущая задержка в тиках
-    public static final Random random = new Random(); // Генератор случайных чисел
-
-    // Косметика
     public static boolean showWings = false;
     public static boolean showHat = false;
     public static boolean showDemonicRays = false;
-
-    public static void toggleColor() {
-        if (clientColor == 0xFFB266FF) clientColor = 0xFFFF3333; 
-        else if (clientColor == 0xFFFF3333) clientColor = 0xFF33FF33; 
-        else if (clientColor == 0xFF33FF33) clientColor = 0xFF33CCFF; 
-        else clientColor = 0xFFB266FF; 
-    }
-
-    public static String getColorName() {
-        if (clientColor == 0xFFB266FF) return "Фиолетовый";
-        if (clientColor == 0xFFFF3333) return "Красный";
-        if (clientColor == 0xFF33FF33) return "Зеленый";
-        if (clientColor == 0xFF33CCFF) return "Голубой";
-        return "Неизвестно";
-    }
+    
+    public static String getColorName() { return "Красный"; }
+    public static void toggleColor() { }
 }
